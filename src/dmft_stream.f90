7bce1120e0e87e0038d5dbf59b4fba35b047af4b
Li Huang <lihuang.dmft@gmail.com>
2021-05-11 20:43:05 +0800

    add comments

--- src/dmft_stream.f90
+++ src/dmft_stream.f90
@@ -19,7 +19,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 02/23/2021 by li huang (created)
-!!!           05/08/2021 by li huang (last modified)
+!!!           05/11/2021 by li huang (last modified)
 !!! purpose :
 !!! status  : unstable
 !!! comment :
@@ -33,7 +33,8 @@
 !! @sub dmft_setup_tasks
 !!
 !! setup control parameters for the dynamical mean-field theory engine.
-!! note that these parameters are extracted from the dmft.in file
+!! note that these parameters are extracted from the `dmft.in` file.
+!! this code can still run without `dmft.in` file
 !!
   subroutine dmft_setup_tasks()
      use constants, only : dp
@@ -126,7 +127,7 @@
 !!
 !! setup dimensional parameters and some real constants for the dynamical
 !! mean-field theory engine. note that these parameters are extracted
-!! from the params.ir file
+!! from the `params.ir` file
 !!
   subroutine dmft_setup_param()
      use constants, only : dp
