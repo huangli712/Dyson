d8016ddcbaa16cd65cba03e349b417128ce502e2
Li Huang <lihuang.dmft@gmail.com>
2021-05-20 22:53:08 +0800

    clean codes

--- src/dmft_print.f90
+++ src/dmft_print.f90
@@ -8,7 +8,7 @@
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 02/23/2021 by li huang (created)
-!!!           05/11/2021 by li huang (last modified)
+!!!           05/20/2021 by li huang (last modified)
 !!! purpose :
 !!! status  : unstable
 !!! comment :
@@ -53,7 +53,7 @@
      write(mystd,'(2X,a)') 'A Modern Dynamical Metal-Field Theory Self-Consistent Equation Solver'
      write(mystd,*)
 
-     write(mystd,'(2X,a)') 'Version: '//V_FULL//' (built at '//__TIME__//" "//__DATE__//')'
+     write(mystd,'(2X,a)') 'Version: '//V_FULL//' (built at '//__TIME__//' '//__DATE__//')'
      write(mystd,'(2X,a)') 'Develop: '//V_AUTH//' ('//V_INST//')'
      write(mystd,'(2X,a)') 'Support: '//V_MAIL
      write(mystd,'(2X,a)') 'License: '//V_GPL3
@@ -196,85 +196,89 @@
 
 ! print lattice structure
      write(mystd,'(2X,a)') '>>> system information from lattice.ir'
-     write(mystd,'(2X,a)') "[system information] -> lattice    -> sorts"
+     write(mystd,'(2X,a)') '[system information] -> lattice    -> sorts'
      write(mystd,'(2X,a)') '-----------------------------------------------------'
      do s=1,nsort
-         write(mystd,'(4X,a6,i3,2X,a8,a3,i3)') "sort :", s, "symbol :", sorts(s), sortn(s)
+         write(mystd,'(4X,a6,i3,2X,a8,a3,i3)') 'sort :', s, 'symbol :', sorts(s), sortn(s)
      enddo ! over s={1,nsort} loop
 
      write(mystd,'(2X,a)') '>>> system information from lattice.ir'
-     write(mystd,'(2X,a)') "[system information] -> lattice    -> atoms"
+     write(mystd,'(2X,a)') '[system information] -> lattice    -> atoms'
      write(mystd,'(2X,a)') '-----------------------------------------------------'
      do s=1,natom
-         write(mystd,'(4X,a6,i3,2X,a8,a3,1X,3f9.6)') "atom :", s, "symbol :", atoms(s), coord(s,:)
+         write(mystd,'(4X,a6,i3,2X,a8,a3,1X,3f9.6)') 'atom :', s, 'symbol :', atoms(s), coord(s,:)
      enddo ! over s={1,natom} loop
      write(mystd,*)
 
 ! print quantum impurities
      write(mystd,'(2X,a)') '>>> system information from sigma.bare'
-     write(mystd,'(2X,a)') "[system information] -> impurities -> sig_l"
+     write(mystd,'(2X,a)') '[system information] -> impurities -> sig_l'
      write(mystd,'(2X,a)') '-----------------------------------------------------'
      do s=1,nsite
          do p=1,nspin
-             write(mystd,'(4X,a10,i3,2X,a6,i3)') "impurity :", s, "spin :", p
+             write(mystd,'(4X,a10,i3,2X,a6,i3)') 'impurity :', s, 'spin :', p
              do i=1,ndim(i_grp(s))
-                 write(mystd,'(4X,a1,i3,2f10.5)') ">", i, real(sig_l(i,i,1,p,s)), real(sig_l(i,i,nmesh,p,s))
+                 write(mystd,'(4X,a1,i3)', advance='no') '>', i
+                 write(mystd,'(2f10.5)', advance='no') real(sig_l(i,i,1,p,s)), real(sig_l(i,i,nmesh,p,s))
+                 write(mystd,'(2X,a)') '(re: w -> 0 and w -> oo)'
              enddo ! over i={1,ndim(i_grp(s))} loop
          enddo ! over p={1,nspin} loop
      enddo ! over s={1,nsite} loop
 
      write(mystd,'(2X,a)') '>>> system information from sigma.dc'
-     write(mystd,'(2X,a)') "[system information] -> impurities -> sigdc"
+     write(mystd,'(2X,a)') '[system information] -> impurities -> sigdc'
      write(mystd,'(2X,a)') '-----------------------------------------------------'
      do s=1,nsite
          do p=1,nspin
-             write(mystd,'(4X,a10,i3,2X,a6,i3)') "impurity :", s, "spin :", p
+             write(mystd,'(4X,a10,i3,2X,a6,i3)') 'impurity :', s, 'spin :', p
              do i=1,ndim(i_grp(s))
-                 write(mystd,'(4X,a1,i3,f10.5)') ">", i, real(sigdc(i,i,p,s))
+                 write(mystd,'(4X,a1,i3)', advance='no') '>', i
+                 write(mystd,'(2f10.5)', advance='no') real(sigdc(i,i,p,s)), aimag(sigdc(i,i,p,s))
+                 write(mystd,'(2X,a)') '(re and im)'
              enddo ! over i={1,ndim(i_grp(s))} loop
          enddo ! over p={1,nspin} loop
      enddo ! over s={1,nsite} loop
 
      write(mystd,'(2X,a)') '>>> system information from maps.ir'
-     write(mystd,'(2X,a)') "[system information] -> impurities -> mappings"
+     write(mystd,'(2X,a)') '[system information] -> impurities -> mappings'
      write(mystd,'(2X,a)') '-----------------------------------------------------'
      do s=1,nsite
-         write(mystd,'(4X,a10,i3,2X,a8,i3)') "impurity :", s, "group  :", i_grp(s)
-         write(mystd,'(4X,a10,i3,2X,a8,i3)') "impurity :", s, "window :", i_wnd(s)
+         write(mystd,'(4X,a10,i3,2X,a8,i3)') 'impurity :', s, 'group  :', i_grp(s)
+         write(mystd,'(4X,a10,i3,2X,a8,i3)') 'impurity :', s, 'window :', i_wnd(s)
      enddo ! over s={1,nsite} loop
      write(mystd,*)
 
 ! print local orbital projectors
      write(mystd,'(2X,a)') '>>> system information from groups.ir'
-     write(mystd,'(2X,a)') "[system information] -> projectors -> groups"
+     write(mystd,'(2X,a)') '[system information] -> projectors -> groups'
      write(mystd,'(2X,a)') '-----------------------------------------------------'
      do s=1,ngrp
-         write(mystd,'(4X,a8,i3)') "group  :", s
-         write(mystd,'(4X,a9,2X,a)') "> shell :", shell(s)
-         write(mystd,'(4X,a9,l3)') ">  corr :", corr(s)
-         write(mystd,'(4X,a9,i3,2X,a2)') ">  site :", site(s), atoms(site(s))
-         write(mystd,'(4X,a9,i3)') ">     l :", l(s)
-         write(mystd,'(4X,a9,i3)') ">  ndim :", ndim(s)
+         write(mystd,'(4X,a8,i3)') 'group  :', s
+         write(mystd,'(4X,a9,2X,a)') '> shell :', shell(s)
+         write(mystd,'(4X,a9,l3)') '>  corr :', corr(s)
+         write(mystd,'(4X,a9,i3,2X,a2)') '>  site :', site(s), atoms(site(s))
+         write(mystd,'(4X,a9,i3)') '>     l :', l(s)
+         write(mystd,'(4X,a9,i3)') '>  ndim :', ndim(s)
      enddo ! over s={1,ngrp} loop
 
      write(mystd,'(2X,a)') '>>> system information from windows.ir'
-     write(mystd,'(2X,a)') "[system information] -> projectors -> windows"
+     write(mystd,'(2X,a)') '[system information] -> projectors -> windows'
      write(mystd,'(2X,a)') '-----------------------------------------------------'
      do s=1,nwnd
-         write(mystd,'(4X,a8,i3)') "window :", s
-         write(mystd,'(4X,a9,i3)') ">  bmin :", bmin(s)
-         write(mystd,'(4X,a9,i3)') ">  bmax :", bmax(s)
-         write(mystd,'(4X,a9,i3)') ">  nbnd :", nbnd(s)
+         write(mystd,'(4X,a8,i3)') 'window :', s
+         write(mystd,'(4X,a9,i3)') '>  bmin :', bmin(s)
+         write(mystd,'(4X,a9,i3)') '>  bmax :', bmax(s)
+         write(mystd,'(4X,a9,i3)') '>  nbnd :', nbnd(s)
      enddo ! over s={1,nwnd} loop
 
      write(mystd,'(2X,a)') '>>> system information from maps.ir'
-     write(mystd,'(2X,a)') "[system information] -> projectors -> mappings"
+     write(mystd,'(2X,a)') '[system information] -> projectors -> mappings'
      write(mystd,'(2X,a)') '-----------------------------------------------------'
      do s=1,ngrp
-         write(mystd,'(4X,a8,i3,2X,a10,i3)') "group  :", s, "impurity :", g_imp(s)
+         write(mystd,'(4X,a8,i3,2X,a10,i3)') 'group  :', s, 'impurity :', g_imp(s)
      enddo ! over s={1,ngrp} loop
      do s=1,nwnd
-         write(mystd,'(4X,a8,i3,2X,a10,i3)') "window :", s, "impurity :", w_imp(s)
+         write(mystd,'(4X,a8,i3,2X,a10,i3)') 'window :', s, 'impurity :', w_imp(s)
      enddo ! over s={1,nwnd} loop
 
      write(mystd,*)
