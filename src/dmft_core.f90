173f7b3643d430c9cdcb7e778b9f3cd252df798e
Li Huang <lihuang.dmft@gmail.com>
2021-06-11 11:35:16 +0800

    implement cal_denmat()

--- src/dmft_core.f90
+++ src/dmft_core.f90
@@ -2078,6 +2078,7 @@
 !!
   subroutine cal_denmat(fermi, eigs, einf)
      use constants, only : dp
+     use constants, only : one, two
      use constants, only : czi, czero
 
      use control, only : axis
@@ -2157,8 +2158,7 @@
              do m=1,nmesh
                  caux = czi * fmesh(m) + fermi
                  do b=1,cbnd
-                     gloc(b,m,s) = gloc(b,m,s) + one / ( caux - eigs(b,m,k,s) )
-                     gloc(b,m,s) = gloc(b,m,s) - one / ( caux - einf(b,k,s) )
+                     gloc(b,m,k,s) = one / ( caux - eigs(b,m,k,s) ) - one / ( caux - einf(b,k,s) )
                  enddo ! over b={1,cbnd} loop
              enddo ! over m={1,nmesh} loop
 
