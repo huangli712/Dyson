99519f1c534a537055566c68ffad00f7aecb2f4c
Li Huang <lihuang.dmft@gmail.com>
2021-06-12 00:02:51 +0800

    update version string

--- src/dmft_control.f90
+++ src/dmft_control.f90
@@ -6,7 +6,7 @@
 !!! type    : modules
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 02/23/2021 by li huang (created)
-!!!           06/11/2021 by li huang (last modified)
+!!!           06/12/2021 by li huang (last modified)
 !!! purpose :
 !!! status  : unstable
 !!! comment :
@@ -325,21 +325,21 @@
 !!
 !! version string, version number + date info. + status info.
 !!
-     character(len=20), public, parameter :: V_FULL = 'v0.5.0 @ 2021.06.10D'
+     character(len=20), public, parameter :: V_FULL = 'v0.5.1 @ 2021.06.12D'
 
 !!
 !! @var V_CURR
 !!
 !! version string, only version number
 !!
-     character(len=06), public, parameter :: V_CURR = 'v0.5.0'
+     character(len=06), public, parameter :: V_CURR = 'v0.5.1'
 
 !!
 !! @var V_DATE
 !!
 !! version string, only date info.
 !!
-     character(len=11), public, parameter :: V_DATE = '2021.06.10'
+     character(len=11), public, parameter :: V_DATE = '2021.06.12'
 
 !!
 !! @var V_STAT
