32b5125202c29068bc869762f671fd1300b9cfab
Li Huang <lihuang.dmft@gmail.com>
2021-07-30 21:53:08 +0800

    implement dmft_util.f90

--- src/dmft_core.f90
+++ src/dmft_core.f90
@@ -2353,39 +2353,3 @@
 
      return
   end subroutine cal_eigsys
-
-!!========================================================================
-!!>>> service subroutines: set 5, fermi-dirac function                 <<<
-!!========================================================================
-
-!!
-!! @fun fermi_dirac
-!!
-!! try to calculate the fermi-dirac function
-!!
-  function fermi_dirac(omega) result(value)
-     use constants, only : dp
-     use constants, only : zero, one
-
-     use control, only : beta
-
-     implicit none
-
-! external arguments
-! frequency point, \omega
-     real(dp), intent(in) :: omega
-
-! result value, return this
-     real(dp) :: value
-
-! check the range of omega to avoid numerical instability
-     if      ( beta * omega >=  600.0_dp ) then
-         value = zero
-     else if ( beta * omega <= -600.0_dp ) then
-         value = one
-     else
-         value = one / ( one + exp( beta * omega ) )
-     endif ! back if ( beta * omega >=  600.0_dp ) block
-
-     return
-  end function fermi_dirac
