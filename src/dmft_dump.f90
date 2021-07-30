f88d6762a50e183f366ba774fcf1d9e0a432405c
Li Huang <lihuang.dmft@gmail.com>
2021-07-31 00:17:34 +0800

    update comments in dmft_dump.f90

--- src/dmft_dump.f90
+++ src/dmft_dump.f90
@@ -546,7 +546,7 @@
 !!
 !! @sub dmft_dump_gamma
 !!
-!! write out the correction for density matrix
+!! write out the dmft correction for dft density matrix.
 !!
   subroutine dmft_dump_gamma(gamma)
      use constants, only : dp
@@ -561,57 +561,59 @@
 
      implicit none
 
-! external arguments
-! local hybridization function
+!! external arguments
+     ! local hybridization function
      complex(dp), intent(in) :: gamma(qbnd,qbnd,nkpt,nspin)
 
-! local variables
-! loop index for spins
+!! local variables
+     ! loop index for spins
      integer :: s
 
-! loop index for k-points
+     ! loop index for k-points
      integer :: k
 
-! loop index for impurity sites
+     ! loop index for impurity sites
      integer :: t
 
-! loop index for bands in band window
+     ! loop index for bands in band window
      integer :: p, q
 
-! number of dft bands for given k-point and spin
+     ! number of dft bands for given k-point and spin
      integer :: cbnd
 
-! band window: start index and end index for bands
+     ! band window: start index and end index for bands
      integer :: bs, be
 
-! open data file: dmft.gamma
+!! [body
+
+     ! open data file: dmft.gamma
      open(mytmp, file='dmft.gamma', form='formatted', status='unknown')
 
-! write parameters
+     ! write parameters
      write(mytmp,'(a9,i4)') '# nkpt : ', nkpt
      write(mytmp,'(a9,i4)') '# nspin: ', nspin
      write(mytmp,'(a9,i4)') '# qbnd : ', qbnd
 
-! write separators
+     ! write separators
      write(mytmp,*)
      write(mytmp,*)
 
-! write body
+     ! write body
      do s=1,nspin
          do k=1,nkpt
 
-! evaluate band window for the current k-point and spin
-! i_wnd(t) returns the corresponding band window for given impurity site t
-!
-! see remarks in cal_nelect() subroutine
+             ! evaluate band window for the current k-point and spin.
+             !
+             ! i_wnd(t) returns the corresponding band window for given
+             ! impurity site t. see remarks in cal_nelect() subroutine.
              t = 1 ! t is fixed to 1
              bs = kwin(k,s,1,i_wnd(t))
              be = kwin(k,s,2,i_wnd(t))
 
-! determine cbnd
+             ! determine cbnd
              cbnd = be - bs + 1
 
-! write data for given spin and k-point
+             ! write data for given spin and k-point
              write(mytmp,'(a,i4)') '# spin:', s
              write(mytmp,'(a,i4,2X,3f16.12)') '# kpt:', k, kmesh(k,1:3)
              write(mytmp,'(3(a,i4,2X))') '# cbnd:', cbnd, 'bs:', bs, 'be:', be
@@ -621,15 +623,17 @@
                  enddo ! over p={1,cbnd} loop
              enddo ! over q={1,cbnd} loop
 
-! write separators
+             ! write separators
              write(mytmp,*)
              write(mytmp,*)
 
          enddo ! over k={1,nkpt} loop
      enddo ! over s={1,nspin} loop
 
-! close data file
+     ! close data file
      close(mytmp)
+
+!! body]
 
      return
   end subroutine dmft_dump_gamma
