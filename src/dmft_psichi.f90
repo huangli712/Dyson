6045e2cdcf4f66b748dea65c7725552eee158b1b
Li Huang <lihuang.dmft@gmail.com>
2021-07-30 23:52:37 +0800

    update comments in dmft_psichi.f90

--- src/dmft_psichi.f90
+++ src/dmft_psichi.f90
@@ -181,8 +181,8 @@
 !!
 !! @sub map_psi_chi
 !!
-!! service subroutine. map a function from Kohn-Sham basis to local
-!! basis. you can call this procedure `projection` or `downfold`
+!! service subroutine. map a matrix function from Kohn-Sham basis to local
+!! basis. you can call this procedure `projection` or `downfold`.
 !!
   subroutine map_psi_chi(cbnd, cdim, nfrq, k, s, t, Mp, Mc)
      use constants, only : dp
@@ -193,43 +193,45 @@
 
      implicit none
 
-! external arguments
-! number of dft bands for given k-point and spin
+!! external arguments
+     ! number of dft bands for given k-point and spin
      integer, intent(in) :: cbnd
 
-! number of correlated orbitals for given impurity site
+     ! number of correlated orbitals for given impurity site
      integer, intent(in) :: cdim
 
-! number of frequency points
+     ! number of frequency points
      integer, intent(in) :: nfrq
 
-! index for k-points
+     ! index for k-points
      integer, intent(in) :: k
 
-! index for spin
+     ! index for spin
      integer, intent(in) :: s
 
-! index for impurity sites
+     ! index for impurity sites
      integer, intent(in) :: t
 
-! input array defined at Kohn-Sham (\psi) basis
+     ! input array defined at Kohn-Sham (\psi) basis
      complex(dp), intent(in)  :: Mp(cbnd,cbnd,nfrq)
 
-! output array defined at local orbital (\chi) basis
+     ! output array defined at local orbital (\chi) basis
      complex(dp), intent(out) :: Mc(cdim,cdim,nfrq)
 
-! local variables
-! loop index for frequency mesh
+!! local variables
+     ! loop index for frequency mesh
      integer :: f
 
-! status flag
+     ! status flag
      integer :: istat
 
-! overlap matrix between local orbitals and Kohn-Sham wave-functions
+     ! overlap matrix between local orbitals and Kohn-Sham wave-functions
      complex(dp), allocatable :: Cp(:,:)
      complex(dp), allocatable :: Pc(:,:)
 
-! allocate memory
+!! [body
+
+     ! allocate memory
      allocate(Cp(cdim,cbnd), stat = istat)
      if ( istat /= 0 ) then
          call s_print_error('map_psi_chi','can not allocate enough memory')
@@ -240,18 +242,20 @@
          call s_print_error('map_psi_chi','can not allocate enough memory')
      endif ! back if ( istat /= 0 ) block
 
-! copy data
+     ! copy data
      Cp = chipsi(1:cdim,1:cbnd,k,s,i_grp(t))
      Pc = psichi(1:cbnd,1:cdim,k,s,i_grp(t))
 
-! downfolding or projection
+     ! downfolding or projection
      do f=1,nfrq
          Mc(:,:,f) = matmul( matmul( Cp, Mp(:,:,f) ), Pc )
      enddo ! over f={1,nfrq} loop
 
-! deallocate memory
+     ! deallocate memory
      if ( allocated(Cp) ) deallocate(Cp)
      if ( allocated(Pc) ) deallocate(Pc)
+
+!! body]
 
      return
   end subroutine map_psi_chi
