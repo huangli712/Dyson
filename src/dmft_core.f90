4fe572d120f5b6656782abaa8e31d95d8a98c417
Li Huang <lihuang.dmft@gmail.com>
2021-08-01 00:50:15 +0800

    update comments in dmft_core.f90

--- src/dmft_core.f90
+++ src/dmft_core.f90
@@ -118,37 +118,39 @@
 
      implicit none
 
-! external arguments
-! number of dft bands for given k-point and spin
+!! external arguments
+     ! number of dft bands for given k-point and spin
      integer, intent(in) :: cbnd
 
-! band window: start index and end index for bands
+     ! band window: start index and end index for bands
      integer, intent(in) :: bs, be
 
-! index for k-points
+     ! index for k-points
      integer, intent(in) :: k
 
-! index for spin
+     ! index for spin
      integer, intent(in) :: s
 
-! self-energy function at Kohn-Sham basis
+     ! self-energy function at Kohn-Sham basis
      complex(dp), intent(in)  :: Sk(cbnd,cbnd,nmesh)
 
-! frequency-dependent effective hamiltonian at given k-point and spin
+     ! frequency-dependent effective hamiltonian at given k-point and spin
      complex(dp), intent(out) :: Hk(cbnd,cbnd,nmesh)
 
-! local variables
-! loop index for frequency mesh
+!! local variables
+     ! loop index for frequency mesh
      integer :: m
 
-! status flag
+     ! status flag
      integer :: istat
 
-! dummy arrays, used to build effective hamiltonian
+     ! dummy arrays, used to build effective hamiltonian
      complex(dp), allocatable :: Em(:)
      complex(dp), allocatable :: Hm(:,:)
 
-! allocate memory
+!! [body
+
+     ! allocate memory
      allocate(Em(cbnd),      stat = istat)
      if ( istat /= 0 ) then
          call s_print_error('cal_sk_hk','can not allocate enough memory')
@@ -159,20 +161,22 @@
          call s_print_error('cal_sk_hk','can not allocate enough memory')
      endif ! back if ( istat /= 0 ) block
 
-! evaluate Em, which is just some dft eigenvalues
+     ! evaluate Em, which is just some dft eigenvalues
      Em = enk(bs:be,k,s)
 
-! convert `Em` to diagonal matrix `Hm`
+     ! convert `Em` to diagonal matrix `Hm`
      call s_diag_z(cbnd, Em, Hm)
 
-! combine `Hm` and `Sk` to build the effective hamiltonian
+     ! combine `Hm` and `Sk` to build the effective hamiltonian
      FREQ_LOOP: do m=1,nmesh
          Hk(:,:,m) = Hm + Sk(:,:,m)
      enddo FREQ_LOOP ! over m={1,nmesh} loop
 
-! deallocate memory
+     ! deallocate memory
      if ( allocated(Em) ) deallocate(Em)
      if ( allocated(Hm) ) deallocate(Hm)
+
+!! body]
 
      return
   end subroutine cal_sk_hk
